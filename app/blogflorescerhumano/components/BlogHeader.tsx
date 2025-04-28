39f9dfaf0e5cb50d4aee321f543a9d02dad08d91
M Daniel  Dantas <contatomarcosdgomes@gmail.com>
2025-04-28 13:05:15 -0300

    feat: Melhora a estrutura do cabeçalho do blog com centralização dos links de navegação

--- app/blogflorescerhumano/components/BlogHeader.tsx
+++ app/blogflorescerhumano/components/BlogHeader.tsx
@@ -6,13 +6,15 @@ import Link from 'next/link';
 const BlogHeader = () => {
   return (
     <header className="bg-gradient-to-r from-blue-600 to-teal-500 text-white shadow-md">
-      <nav className="container mx-auto px-4 py-4 flex justify-between items-center">
+      {/* Alterado: removido justify-between, adicionado relative */}
+      <nav className="container mx-auto px-4 py-4 flex items-center relative">
         {/* Link para a página inicial do blog */}
         <Link href="/blogflorescerhumano" legacyBehavior>
           <a className="text-2xl font-bold hover:text-gray-200">Blog Florescer Humano</a>
         </Link>
-        {/* Links de navegação do blog */}
-        <div className="space-x-4">
+        {/* Links de navegação do blog - Centralizados */}
+        {/* Alterado: adicionado absolute, left-1/2, transform, -translate-x-1/2 */}
+        <div className="absolute left-1/2 transform -translate-x-1/2 space-x-4">
           <Link href="/blogflorescerhumano/categorias" legacyBehavior><a className="hover:text-gray-200">Categorias</a></Link>
           <Link href="/blogflorescerhumano/artigos" legacyBehavior><a className="hover:text-gray-200">Artigos</a></Link>
           <Link href="/blogflorescerhumano/sobre" legacyBehavior><a className="hover:text-gray-200">Sobre</a></Link>
@@ -20,7 +22,15 @@ const BlogHeader = () => {
           {/* Adiciona links para as novas páginas */}
           <Link href="/blogflorescerhumano/materiais" legacyBehavior><a className="hover:text-gray-200">Materiais</a></Link>
           <Link href="/blogflorescerhumano/midias" legacyBehavior><a className="hover:text-gray-200">Mídias</a></Link>
+          {/* Botão movido para fora deste div */}
         </div>
+        {/* Botão para voltar ao site principal - Empurrado para a direita */}
+        {/* Alterado: adicionado ml-auto */}
+        <Link href="/" legacyBehavior>
+          <a className="ml-auto px-3 py-1 border border-white rounded-md hover:bg-white hover:text-blue-600 transition-colors">
+            Site Psi Daniel Dantas
+          </a>
+        </Link>
       </nav>
     </header>
   );
