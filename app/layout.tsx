20e8d3d93d75901bea6cf6b4a82ecae962ebfc5c
M Daniel  Dantas <contatomarcosdgomes@gmail.com>
2025-05-29 19:42:23 -0300

    refactor: atualizar ícones e metadados para usar os do blog

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,39 +56,41 @@ export const metadata: Metadata = {  // Metadados básicos
     creator: "@psidanieldantas", // Se tiver um perfil no Twitter, incluir aqui
   },
   // Site já verificado via provedor de domínio no Google Search Console
-
-  // Favicons, Manifest and Other related meta for the main site
+  // Favicons, Manifest and Other related meta - agora usando os mesmos do blog
   icons: {
     icon: [
-      { url: '/favicons-site-principal/favicon.ico', sizes: 'any', type: 'image/x-icon' },
-      { url: '/favicons-site-principal/favicon-16x16.png', type: 'image/png', sizes: '16x16' },
-      { url: '/favicons-site-principal/favicon-32x32.png', type: 'image/png', sizes: '32x32' },
-      { url: '/favicons-site-principal/favicon-96x96.png', type: 'image/png', sizes: '96x96' },
-      { url: '/favicons-site-principal/android-icon-36x36.png', type: 'image/png', sizes: '36x36' },
-      { url: '/favicons-site-principal/android-icon-48x48.png', type: 'image/png', sizes: '48x48' },
-      { url: '/favicons-site-principal/android-icon-72x72.png', type: 'image/png', sizes: '72x72' },
-      { url: '/favicons-site-principal/android-icon-96x96.png', type: 'image/png', sizes: '96x96' },
-      { url: '/favicons-site-principal/android-icon-144x144.png', type: 'image/png', sizes: '144x144' },
-      { url: '/favicons-site-principal/android-icon-192x192.png', type: 'image/png', sizes: '192x192' },
+      { url: '/blogflorescerhumano/favicon-blog-florescer-humano/favicon.ico', sizes: 'any', type: 'image/x-icon' },
+      { url: '/blogflorescerhumano/favicon-blog-florescer-humano/favicon-16x16.png', type: 'image/png', sizes: '16x16' },
+      { url: '/blogflorescerhumano/favicon-blog-florescer-humano/favicon-32x32.png', type: 'image/png', sizes: '32x32' },
+      { url: '/blogflorescerhumano/favicon-blog-florescer-humano/favicon-96x96.png', type: 'image/png', sizes: '96x96' },
+      { url: '/blogflorescerhumano/favicon-blog-florescer-humano/android-icon-36x36.png', type: 'image/png', sizes: '36x36' },
+      { url: '/blogflorescerhumano/favicon-blog-florescer-humano/android-icon-48x48.png', type: 'image/png', sizes: '48x48' },
+      { url: '/blogflorescerhumano/favicon-blog-florescer-humano/android-icon-72x72.png', type: 'image/png', sizes: '72x72' },
+      { url: '/blogflorescerhumano/favicon-blog-florescer-humano/android-icon-96x96.png', type: 'image/png', sizes: '96x96' },
+      { url: '/blogflorescerhumano/favicon-blog-florescer-humano/android-icon-144x144.png', type: 'image/png', sizes: '144x144' },
+      { url: '/blogflorescerhumano/favicon-blog-florescer-humano/android-icon-192x192.png', type: 'image/png', sizes: '192x192' },
     ],
     apple: [
-      { url: '/favicons-site-principal/apple-icon.png', type: 'image/png' },
-      { url: '/favicons-site-principal/apple-icon-57x57.png', type: 'image/png', sizes: '57x57' },
-      { url: '/favicons-site-principal/apple-icon-60x60.png', type: 'image/png', sizes: '60x60' },
-      { url: '/favicons-site-principal/apple-icon-72x72.png', type: 'image/png', sizes: '72x72' },
-      { url: '/favicons-site-principal/apple-icon-76x76.png', type: 'image/png', sizes: '76x76' },
-      { url: '/favicons-site-principal/apple-icon-114x114.png', type: 'image/png', sizes: '114x114' },
-      { url: '/favicons-site-principal/apple-icon-120x120.png', type: 'image/png', sizes: '120x120' },
-      { url: '/favicons-site-principal/apple-icon-144x144.png', type: 'image/png', sizes: '144x144' },
-      { url: '/favicons-site-principal/apple-icon-152x152.png', type: 'image/png', sizes: '152x152' },
-      { url: '/favicons-site-principal/apple-icon-180x180.png', type: 'image/png', sizes: '180x180' },
-      { url: '/favicons-site-principal/apple-icon-precomposed.png', type: 'image/png', rel: 'apple-touch-icon-precomposed' },
+      { url: '/blogflorescerhumano/favicon-blog-florescer-humano/apple-icon.png', type: 'image/png' },
+      { url: '/blogflorescerhumano/favicon-blog-florescer-humano/apple-icon-57x57.png', type: 'image/png', sizes: '57x57' },
+      { url: '/blogflorescerhumano/favicon-blog-florescer-humano/apple-icon-60x60.png', type: 'image/png', sizes: '60x60' },
+      { url: '/blogflorescerhumano/favicon-blog-florescer-humano/apple-icon-72x72.png', type: 'image/png', sizes: '72x72' },
+      { url: '/blogflorescerhumano/favicon-blog-florescer-humano/apple-icon-76x76.png', type: 'image/png', sizes: '76x76' },
+      { url: '/blogflorescerhumano/favicon-blog-florescer-humano/apple-icon-114x114.png', type: 'image/png', sizes: '114x114' },
+      { url: '/blogflorescerhumano/favicon-blog-florescer-humano/apple-icon-120x120.png', type: 'image/png', sizes: '120x120' },
+      { url: '/blogflorescerhumano/favicon-blog-florescer-humano/apple-icon-144x144.png', type: 'image/png', sizes: '144x144' },
+      { url: '/blogflorescerhumano/favicon-blog-florescer-humano/apple-icon-152x152.png', type: 'image/png', sizes: '152x152' },
+      { url: '/blogflorescerhumano/favicon-blog-florescer-humano/apple-icon-180x180.png', type: 'image/png', sizes: '180x180' },
+      { url: '/blogflorescerhumano/favicon-blog-florescer-humano/apple-icon-precomposed.png', type: 'image/png', rel: 'apple-touch-icon-precomposed' },
     ],
-    shortcut: ['/favicons-site-principal/favicon.ico'],
+    shortcut: ['/blogflorescerhumano/favicon-blog-florescer-humano/favicon.ico'],
   },
-  manifest: '/favicons-site-principal/manifest.json',
+  manifest: '/blogflorescerhumano/favicon-blog-florescer-humano/manifest.json',
   other: {
-    'msapplication-config': '/favicons-site-principal/browserconfig.xml',
+    'msapplication-TileColor': '#ffffff',
+    'msapplication-TileImage': '/blogflorescerhumano/favicon-blog-florescer-humano/ms-icon-144x144.png',
+    'msapplication-config': '/blogflorescerhumano/favicon-blog-florescer-humano/browserconfig.xml',
+    'theme-color': '#ffffff'
   },
   
   // Outros metadados
